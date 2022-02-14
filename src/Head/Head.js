57c155ae5a43069e7751e3416ec090470ec2b76c
laiaqwq <64451522+laiaqwq@users.noreply.github.com>
2022-02-14 18:23:33 +0800

    Update Head.js

--- src/Head/Head.js
+++ src/Head/Head.js
@@ -6,18 +6,18 @@ class Head extends Component {
             <div>
                 <div className="mdui-appbar">
                     <div className="mdui-toolbar mdui-color-theme">
-                        <a href="javascript:;" className="mdui-btn mdui-btn-icon">
+                        <a href="/" className="mdui-btn mdui-btn-icon">
                             <i className="mdui-icon material-icons">menu</i>
                         </a>
                         <span className="mdui-typo-title">Title</span>
                         <div className="mdui-toolbar-spacer"></div>
-                        <a href="javascript:;" className="mdui-btn mdui-btn-icon">
+                        <a href="/" className="mdui-btn mdui-btn-icon">
                             <i className="mdui-icon material-icons">search</i>
                         </a>
-                        <a href="javascript:;" className="mdui-btn mdui-btn-icon">
+                        <a href="/" className="mdui-btn mdui-btn-icon">
                             <i className="mdui-icon material-icons">refresh</i>
                         </a>
-                        <a href="javascript:;" className="mdui-btn mdui-btn-icon">
+                        <a href="/" className="mdui-btn mdui-btn-icon">
                             <i className="mdui-icon material-icons">more_vert</i>
                         </a>
                     </div>
