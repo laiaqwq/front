25f2656471273806217b24ac69b94ea3fa7f8dc5
laiaqwq <64451522+laiaqwq@users.noreply.github.com>
2022-03-05 03:17:40 +0000

    Deploying to gh-pages from  @ 1e664d4b98b3fe1d9d024a19f730c873d0f25c92 🚀

--- js/jsonp.js
+++ js/jsonp.js
@@ -1,4 +1,9 @@
 function getTip(data) {
+    data = data[1];
     console.log(data);
-    window.setGoogleSearchTip(data);
+    let TipArray = [];
+    for (let index=0; index < data.length; index++) {
+        TipArray[index] = data[index][0];
+    }
+    window.setGoogleSearchTip(TipArray);
 }
